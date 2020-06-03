7ccd827a23af252b70e4fa0c5857a712795edcc0
xgqfrms <xgqfrms@outlook.com>
2020-06-03 18:41:16 +0800

    NODE_ENV bug

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,34 +1,41 @@
-// plugins
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const merge = require('webpack-merge');
 
-// built-in plugins
-const webpack = require('webpack');
+const baseConfig = require('./webpack.base.config');
+const devConfig = require('./webpack.dev.config');
+const proConfig = require('./webpack.prod.config');
 
-const path = require('path');
+const log = console.log;
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'webpack-plugin-demo.bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ]
-  },
-  devtool: 'inline-source-map',
-  mode: 'development',
-  // mode: 'production',
-  plugins: [
-    new webpack.ProgressPlugin(),
-  ],
+// log(`process.env =\n`, process.env)
+log(`process.argv =\n`, process.argv)
+// [
+//   '/Users/xgqfrms-mbp/.nvm/versions/node/v10.16.3/bin/node',
+//   '/Users/xgqfrms-mbp/Documents/GitHub/typescript-in-action/node_modules/.bin/webpack',
+//   '--mode=production',
+//   '--config',
+//   './build/webpack.config.js'
+// ]
+
+module.exports = (env, args) => {
+  log(`process.env.NODE_ENV =`, process.env.NODE_ENV)
+  log(`env =`, env)
+  log(`args =\n`, args)
+  // process.env.NODE_ENV = undefined
+  // env = undefined
+  // args =
+  // {
+  //   _: [],
+  //   cache: null,
+  //   bail: null,
+  //   profile: null,
+  //   color: { level: 3, hasBasic: true, has256: true, has16m: true },
+  //   colors: { level: 3, hasBasic: true, has256: true, has16m: true },
+  //   mode: 'production',
+  //   config: './build/webpack.config.js',
+  //   'info-verbosity': 'info',
+  //   infoVerbosity: 'info',
+  //   '$0': '/Users/xgqfrms-mbp/Documents/GitHub/typescript-in-action/node_modules/.bin/webpack'
+  // }
+  const config = args.mode === 'development' ? devConfig : proConfig;
+  return merge(baseConfig, config);
 };
