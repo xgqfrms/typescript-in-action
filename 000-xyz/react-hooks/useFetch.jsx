ba3ec4402cd78b459881996e20a1a07f24da8400
xgqfrms <xgqfrms@outlook.com>
2020-06-09 00:01:52 +0800

    Create useFetch.jsx

---
+++ 000-xyz/react-hooks/useFetch.jsx
@@ -0,0 +1,65 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-06-08
+ * @modified
+ *
+ * @description useFetch 自定义 react hooks
+ * @augments
+ * @example
+ * @link
+ *
+ */
+
+import {
+  useEffect,
+  useState,
+  useRef,
+} from "react";
+
+const log = console.log;
+
+const useFetch = (url = ``) => {
+  const isCurrent = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+  });
+  useEffect(() => {
+    return () => {
+      // component unmount
+      isCurrent.current = false;
+    }
+  }, []);
+  useEffect(() => {
+    setState(state => ({
+      data: state.date,
+      loading: true,
+    }));
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      log(`json`, json)
+      if(isCurrent.current) {
+        setState({
+          data: json.date,
+          loading: false,
+        })
+      }
+    })
+    return () => {
+      cleanup
+    }
+  }, [url, setState]);
+  return state;
+}
+
+export default useFetch;
+
+export {
+  useFetch,
+};
