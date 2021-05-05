0632d4f31512c329898b008d24afa50a874c1cf2
xgqfrms <xgqfrms@outlook.com>
2021-05-05 21:33:13 +0800

    Create mjs.js

---
+++ ts-test/mjs.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2021-03-0
+ * @modified
+ *
+ * @description
+ * @description
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @time O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/???/
+ * @link https://leetcode-cn.com/problems/???/
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+const log = console.log;
+
+// Generator
+const  = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+export default ;
+export {
+  ,
+};
+
+/*
+
+// error
+const test1 = isValid(`((}}`);
+// ok
+const test2 = isValid(`()[]{}`);
+
+log(`❌test =`, test1 ? `✅` : `❌`);
+log(`✅test ok =`, test2 ? `✅` : `❌`);
+
+*/
