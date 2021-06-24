7ab21369727ebc89ff4922170fef0345d548c2fe
xgqfrms <xgqfrms@outlook.com>
2021-06-24 18:25:12 +0800

    ts 类型推断

---
+++ ts-test/type-inference.ts
@@ -0,0 +1,8 @@
+let helloWorld = "Hello World";
+//  ^?
+
+// 类型推断
+helloWorld = 123;
+// Type 'number' is not assignable to type 'string'.
+
+// https://www.typescriptlang.org/play?ssl=6&ssc=53&pln=1&pc=1#code/DYUwLgBAFizA9gdXgJ2AEwgXggIgBKwITJrq4DcAUAPQ0QQB6A-FbfYN4+g0eqAVxoLWmVGHCSoM2CAEYATAGZqdCABUAngAcQEAOQA7AK4BbAEYgUmiAEsAzhG3xIAQ0uXzAc233DoCGHje1GzUswFHNtF00AOiA
