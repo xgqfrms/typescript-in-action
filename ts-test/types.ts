b2fc01d437401f530b0f72911ad9b99bb96f2f97
xgqfrms <xgqfrms@outlook.com>
2021-06-25 14:58:24 +0800

    Create types.ts

---
+++ ts-test/types.ts
@@ -0,0 +1,23 @@
+
+
+/*
+
+There are already a small set of primitive types available in JavaScript:
+boolean, bigint, null, number, string, symbol, and undefined,
+which you can use in an interface.
+
+TypeScript extends this list with a few more, such as
+any (allow anything),
+unknown (ensure someone using this type declares what the type is),
+never (it’s not possible that this type could happen), and
+void (a function which returns undefined or has no return value).
+
+*/
+
+// JS: boolean, bigint, null, number, string, symbol, and undefined,
+
+// TS: any, unknown, never, void
+
+
+
+
