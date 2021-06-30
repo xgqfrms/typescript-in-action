5332e778e9d4ce17d3a33261d06ba0da8d26b918
xgqfrms <xgqfrms@outlook.com>
2021-06-30 23:15:35 +0800

    interface 字段后面添加 分号 ✅
    interface 字段后面添加 分号 ✅

--- ts-test/interface.ts
+++ ts-test/interface.ts
@@ -1,15 +1,26 @@
-/* 
+/*
 
-// interface 字段后面的分号， 是可选的，但是建议加上！
-export interface CanvasFingerprint { 
+// interface 字段后面的逗号， 是可选的，但是建议加上！
+export interface CanvasFingerprint {
   winding: boolean,
   geometry: string,
   text: string,
 }
 
  */
+/*
+
+// interface 字段后面的分号 ✅， 是可选的，但是建议加上！
+export interface CanvasFingerprint {
+  winding: boolean;
+  geometry: string;
+  text: string;
+}
+
+ */
+
 // interface 是 TS 校验自定义类型使用的，但是最终不会被编译出 js 代码
-export interface CanvasFingerprint { 
+export interface CanvasFingerprint {
   winding: boolean
   geometry: string
   text: string
@@ -20,3 +31,33 @@ const obj: CanvasFingerprint = {
   geometry: 'string',
   text: 'string',
 };
+
+
+/*
+
+// .js
+
+const obj = {
+    winding: true,
+    geometry: 'string',
+    text: 'string',
+};
+export {};
+
+
+*/
+
+/*
+
+// .d.ts
+
+// 分号 ✅
+
+export interface CanvasFingerprint {
+    winding: boolean;
+    geometry: string;
+    text: string;
+}
+
+
+*/
