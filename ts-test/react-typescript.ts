98fa6fa95cef238a1c2e9fcf20f3d945d530e7d8
xgqfrms <xgqfrms@outlook.com>
2021-07-08 17:30:57 +0800

    Create react-typescript.ts

---
+++ ts-test/react-typescript.ts
@@ -0,0 +1,21 @@
+// react + typescript
+
+/*
+
+
+```sh
+$ npx create-react-app my-app
+
+$ npm init react-app my-app
+
+$ yarn create react-app my-app
+
+```
+
+https://create-react-app.dev/docs/getting-started/#creating-an-app
+
+
+https://www.typescriptlang.org/docs/
+
+*/
+
